d5a50a1c75a65c297ce3b5a72c61a69c12904185
Sunwoo Park <75346781+Lunatic2339@users.noreply.github.com>
2022-12-06 02:56:53 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int checkValid(std::string str)
+{
+	for (char c : str)
+	{
+
+	}
+	return 1;
+}
+
+int main()
+{
+	int num;
+	std::cin >> num;
+	std::string str;
+	for (int i = 0; i < num; ++i)
+	{
+		int valid = 0;
+		cin >> str;
+		for (char c : str)
+		{
+			cout << '(';
+		}
+	}
+	return 0;
+}
+
+
+
+	
