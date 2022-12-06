e5bd11e822426fdad3ccf317aedde414ec1a8530
Sunwoo Park <75346781+Lunatic2339@users.noreply.github.com>
2022-12-06 21:33:05 +0900

    Add files via upload

--- fraction.cpp
+++ fraction.cpp
@@ -1,40 +1,151 @@
 #include "fraction.h"
 
+// fraction class default constructor
 fraction::fraction()
-	: denom(1), num(1)
 {
+	nArr.push_back(number(0,0,0));
 }
 
+//  fraction class custom constructor 1
 fraction::fraction(int denominator, int numerator)
-	: denom(denominator), num(numerator)
 {
+	nArr.push_back(number(denominator, numerator, 1));
 }
 
+//  fraction class custom constructor 2
 fraction::fraction(int denominator, int numerator, int root)
-	: denom(denominator), num(numerator), root(root)
 {
+	nArr.push_back(number(denominator, numerator, root));
 }
 
+//  fraction class copy constructor
+fraction::fraction(const fraction& other)
+{
+	nArr = other.nArr;
+}
+
+
+
+// fraction class '+' operator overloading
 fraction fraction::operator+(const fraction& frac)
 {
-	int x = gcd(this->denom * frac.denom, this->num * frac.denom + frac.num * this->denom);
-	return fraction((this->denom * frac.denom)/x, (this->num * frac.denom + frac.num * this->denom)/x);
+	fraction result = *this;
+	for (number n : frac.nArr)
+	{
+		int con = 0;
+		for (number& m : result.nArr)
+		{
+			if (n.root == m.root) m = m + n;
+			else ++con;
+		}
+		if (con == result.nArr.size()) result.nArr.push_back(n);
+	}
+	return result;
 }
 
 fraction fraction::operator-(const fraction& frac)
 {
-	int x = gcd(this->denom * frac.denom, this->num * frac.denom + frac.num * this->denom);
-	return fraction((this->denom * frac.denom) / x, (this->num * frac.denom - frac.num * this->denom) / x);
+	fraction result = *this;
+	for (number n : frac.nArr)
+	{
+		int con = 0;
+		for (number& m : result.nArr)
+		{
+			if (n.root == m.root) m = m - n;
+			else ++con;
+		}
+		if (con == result.nArr.size()) result.nArr.push_back(-n);
+	}
+	return result;
 }
 
-fraction fraction::operator*(const fraction& frac)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// number inner class default constrctor
+fraction::number::number()
+	: denom(0), num(0), root(0)
 {
-	int x = gcd(this->denom * frac.denom, this->num * frac.num);
-	return fraction((this->denom * frac.denom) / x, (this->num * frac.num) / x);
+	sign = 1;
 }
 
-fraction fraction::operator/(const fraction& frac)
+// number inner class custom constrctor
+fraction::number::number(int x, int y, unsigned int z)
+	: denom(abs_val(x)), num(abs_val(y)), root(z)
 {
-	int x = gcd(this->denom * frac.num, this->num * frac.denom);
-	return fraction((this->denom * frac.num) / x, (this->num * frac.denom) / x);
+	if (x * y > 0) sign = 1;
+	else if (x * y < 0) sign = -1;
 }
+
+// number inner class '+' operator overloading
+fraction::number fraction::number::operator+(number n)
+{
+	if (this->root == n.root)
+	{
+		if (this->sign == 1 && n.sign == 1)
+		{
+			int x = gcd(this->denom * n.denom, this->num * n.denom + n.num * this->denom);
+			number result((this->denom * n.denom) / x, (this->num * n.denom + n.num * this->denom) / x, this->root);
+			return result;
+		}
+		else if (this->sign == 1 && n.sign == -1)
+		{
+			int x = gcd(this->denom * n.denom, this->num * n.denom - n.num * this->denom);
+			number result((this->denom * n.denom) / x, (this->num * n.denom - n.num * this->denom) / x, this->root);
+			return result;
+		}
+		else if (this->sign == -1 && n.sign == 1)
+		{
+			int x = gcd(this->denom * n.denom, n.num * this->denom - this->num * n.denom);
+			number result((this->denom * n.denom) / x, (n.num * this->denom - this->num * n.denom) / x, this->root);
+			return result;
+		}
+		else if (this->sign == -1 && n.sign == -1)
+		{
+			int x = gcd(this->denom * n.denom, -this->num * n.denom - n.num * this->denom);
+			number result((this->denom * n.denom) / x, (-this->num * n.denom - n.num * this->denom) / x, this->root);
+			return result;
+		}
+	}
+
+	else throw "not same root value";
+}
+
+fraction::number fraction::number::operator-(number n)
+{
+	if (this->root == n.root)
+	{
+		int x = gcd(this->denom * n.denom, this->num * n.denom - n.num * this->denom);
+		number result((this->denom * n.denom) / x, (this->num * n.denom + n.num * this->denom) / x, this->root);
+	}
+	else throw "not same root value";
+}
+
+fraction::number fraction::number::operator-()
+{
+	return number();
+}
+
